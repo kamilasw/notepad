4e084b685215d12d2ad362fd7f802189b70c735c
Kamila Świercz <01189013@pw.edu.pl>
2025-04-21 13:14:29 +0200

    change gitignore

--- todolist/obj/Debug/net8.0-windows/todolist.AssemblyInfo.cs
+++ todolist/obj/Debug/net8.0-windows/todolist.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("todolist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1021e7787454f42ebddb4ea363b53d8b8b4f5ccd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3986bff8d0d918e87ce62e3e601f9587b7f41061")]
 [assembly: System.Reflection.AssemblyProductAttribute("todolist")]
 [assembly: System.Reflection.AssemblyTitleAttribute("todolist")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
